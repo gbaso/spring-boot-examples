ca3c677d9b1de6a43f03301ec8315f9b047a4501
Giacomo Baso <ganondolf@gmail.com>
2022-01-09 15:53:55 +0100

    initialize application

---
+++ src/test/java/com/github/gbaso/examples/SpringBootExamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.gbaso.examples;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
