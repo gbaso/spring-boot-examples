bafa1da5c37b682ea0209aaf411ea3c6a3d9f5ca
Giacomo Baso <ganondolf@gmail.com>
2022-02-21 19:38:46 +0100

    remove unnecessary @Repository annotation

--- src/main/java/com/github/gbaso/examples/jpa/repositories/CustomerOrderRepository.java
+++ src/main/java/com/github/gbaso/examples/jpa/repositories/CustomerOrderRepository.java
@@ -2,9 +2,6 @@ package com.github.gbaso.examples.jpa.repositories;
 
 import com.github.gbaso.examples.jpa.entities.CustomerOrder;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
 public interface CustomerOrderRepository extends BaseEntityRepository<CustomerOrder> {
     
 }
--- src/main/java/com/github/gbaso/examples/jpa/repositories/CustomerRepository.java
+++ src/main/java/com/github/gbaso/examples/jpa/repositories/CustomerRepository.java
@@ -2,9 +2,6 @@ package com.github.gbaso.examples.jpa.repositories;
 
 import com.github.gbaso.examples.jpa.entities.Customer;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
 public interface CustomerRepository extends BaseEntityRepository<Customer> {
     
 }
