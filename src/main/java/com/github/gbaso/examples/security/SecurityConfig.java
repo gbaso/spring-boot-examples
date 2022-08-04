3d4fc47665717bb89daa77ac500a4fe9a0e671b9
Giacomo Baso <ganondolf@gmail.com>
2022-08-04 18:15:45 +0200

    allow all requests in spring security

---
+++ src/main/java/com/github/gbaso/examples/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.github.gbaso.examples.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .cors(Customizer.withDefaults())
+            .csrf(CsrfConfigurer::disable)
+            .authorizeRequests(auth -> auth.anyRequest().permitAll())
+            .build();
+    }
+    
+}
